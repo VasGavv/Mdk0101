278989b1478b845fea55ad297709f775d0dba83d
VasGavv <150891028+VasGavv@users.noreply.github.com>
2026-09-12 14:04:41 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напиши ширину"); 
+int a = int.Parse(Console.ReadLine()); 
+Console.Write("Напиши Высоту: ");
+int b = int.Parse(Console.ReadLine()); 
+
+int c = a * b;
+int c_2 = (a * 2) + (b * 2);
+Console.WriteLine($"Площадь: {c}");
+
+
+Console.WriteLine($"Периметр: {c_2}");
